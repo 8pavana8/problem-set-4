bab52e113bf1b42548bcdaff6372c6536d0dba8f
8pavana8 <43757530+8pavana8@users.noreply.github.com>
2018-11-20 13:09:59 -0500

    Update problem-set-4.js

--- problem-set-4.js
+++ problem-set-4.js
@@ -1,33 +1,23 @@
-/*
- * Hello. 2 points.
- */
-
 function hello() {
 
   // WRITE YOUR EXERCISE 1 CODE HERE
-  document.getElementByID ("output1).innerHTML="Hello, AP Computer Science Principles!";
-
+   document.getElementById ("output1").innerHTML="Hello, AP Computer Science Principles!";
   ////////////////// DO NOT MODIFY
   check("hello"); // DO NOT MODIFY
   ////////////////// DO NOT MODIFY
 }
-
 /*
  * Hello, Again. 3 points.
  */
-
 function helloAgain() {
-
   //////////// DO NOT MODIFY
   let name; // DO NOT MODIFY
   //////////// DO NOT MODIFY
-
   // Use the name variable declared above to store the user's response. You
   // do not need to re-declare it, only assign it a value.
-
   // WRITE YOUR EXERCISE 2 CODE HERE
   name=prompt ("Please Enter Your Name");
-  document.getElementById ("output2").innerHTML="Hello, + name + "!";
+  document.getElementById ("output2").innerHTML="Hello, "+ name + "!";
 
   ///////////////////////////// DO NOT MODIFY
   check("helloAgain", name); // DO NOT MODIFY
@@ -48,92 +38,72 @@ function celsius() {
   // (inclusive), and rounds this value to 2 decimal places.
 
   // WRITE YOUR EXERCISE 3 CODE HERE
-let fahrenheit = ((cels*9)/5 + 32).toFixed(2);
-document.getElementById ("output3").innerHTML= cels + "degrees Celsius equals" + fahrenheit + degrees Fahrenheit.";
+  let fahrenheit = ((cels*9)/5 + 32).toFixed(2);
+  document.getElementById ("output3").innerHTML= cels + "degrees Celsius equals" + fahrenheit + "degrees Fahrenheit.";
   ////////////////////////// DO NOT MODIFY
   check("celsius", cels); // DO NOT MODIFY
   ////////////////////////// DO NOT MODIFY
 }
-
 /*
  * Fahrenheit. 5 points.
  */
-
 function fahrenheit() {
-
   //////////////////////////////////////////////////////// DO NOT MODIFY
   let fahr = ((Math.random() * 1001) - 100).toFixed(2); // DO NOT MODIFY
   //////////////////////////////////////////////////////// DO NOT MODIFY
-
   // The above code generates a random number between -100 and 1000
   // (inclusive), and rounds this value to 2 decimal places.
-
   // WRITE YOUR EXERCISE 4 CODE HERE
-let celsius = ((fahr-32)*5/9).tofixed(2);
+  let celsius = ((fahr-32)*5/9).tofixed(2);
 document.getElementById ("output4").innerHTML=fahr + "degrees Fahrenheit equals" + celsius + "degrees Celsius.";
-  ///////////////////////////// DO NOT MODIFY
   check("fahrenheit", fahr); // DO NOT MODIFY
   ///////////////////////////// DO NOT MODIFY
 }
-
 /*
  * Inches. 5 points.
  */
-
 function inches() {
-
   /////////////////////////////////////////////////////// DO NOT MODIFY
   let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
   /////////////////////////////////////////////////////// DO NOT MODIFY
-
   // You are free to modify the value of inches, which you'll likely
   // need to do. Please do not modify the value of input.
-
   ////////////////////// DO NOT MODIFY
   let inches = input; // DO NOT MODIFY
   ////////////////////// DO NOT MODIFY
-
   // WRITE YOUR EXERCISE 5 CODE HERE
-let miles=Math.floor (inches/63360);
-let yards=Math.floor ((inches - (miles*63360))/36);
-let feet+Math.floor  ((inches- (miles*63360) - (yards*36))12;
-inches=Math.floor  ((inches - (miles*63360) - (yards*36) - (feet*12))/1);
-document.getElementById ("output5").innerHTML="Miles:" + miles + "<br/>Yards: + yards + "<br/>Feet:" + feet + <br/>Inches:" +inches;
-  
+  let miles=Math.floor(inches/63360);
+  let yards=Math.floor((inches - (miles*63360))/36);
+  let feet=Math.floor((inches- (miles*63360) - (yards*36))/12);
+  inches=Math.floor((inches - (miles*63360) - (yards*36) - (feet*12))/1);
+  document.getElementById("output5").innerHTML=("Miles: "+miles+"</br>Yards: "+yards+"</br>Feet: "+feet+"</br>Inches: "+inches);
 
-  
   ////////////////////////// DO NOT MODIFY
   check("inches", input); // DO NOT MODIFY
   ////////////////////////// DO NOT MODIFY
 }
-
 /*
  * Centimeters. 5 points.
  */
-
 function centimeters() {
-
   /////////////////////////////////////////////////////// DO NOT MODIFY
   let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
   /////////////////////////////////////////////////////// DO NOT MODIFY
-
   // You are free to modify the value of centimeters, which you'll
   // likely need to do. Please do not modify the value of input.
-
   /////////////////////////// DO NOT MODIFY
   let centimeters = input; // DO NOT MODIFY
   /////////////////////////// DO NOT MODIFY
-
   // WRITE YOUR EXERCISE 6 CODE HERE
-  let kmeters=Math.floor (centimeters/100000);
-  let meters=Math.floor((centimeters - (kmeters*100000))/100);
-  centimeters=Math.floor ((centimeters - (kmeters*100000) - (meters*100))/1);
-  document.getElementById("output6").innerHTML= "Kilometers: +kmeters +"<br/>Meters:" + meters + "<br/>Centimeters: " + centimeters;
+  let kmeters=Math.floor(centimeters/100000);
+let meters=Math.floor((centimeters - (kmeters*100000))/100);
+centimeters=Math.floor((centimeters - (kmeters*100000) - (meters*100))/1);
+document.getElementById("output6").innerHTML= "Kilometers:" +kmeters +"<br/>Meters:" + meters + "<br/>Centimeters: " + centimeters;
 
   /////////////////////////////// DO NOT MODIFY
   check("centimeters", input); // DO NOT MODIFY
   /////////////////////////////// DO NOT MODIFY
-}
+
 
 /*
  * Fluid Ounces. 5 points.
@@ -153,71 +123,59 @@ function fluidOunces() {
   /////////////////////////// DO NOT MODIFY
 
   // WRITE YOUR EXERCISE 7 CODE HERE
-let gallon=Math.floor(fluidOunces/128);
-let quart=Math.floor((fluidOunces - (gallon*128))/32;
-let pint=Math.floor((fluidOunces - (gallon*128) - (quart*32) - (pint*16))/8);
-fluidOunces=Math.floor- (gallon*128) - (quart*32) - (pint*16) - (cup*8))/1);
+  let gallon=Math.floor(fluidOunces/128);
+let quart=Math.floor((fluidOunces - (gallon*128))/32);
+let pint=Math.floor((fluidOunces - (gallon*128) - (quart*32))/16);
+let cup=Math.floor((fluidOunces - (gallon*128) - (quart*32) - (pint*16))/8);
+let fluidOunces=Math.floor(fluidOunces - (gallon*128) - (quart*32) - (pint*16) - (cup*8));
+
  document.getElementById("output7").innerHTML= "Gallons: " + gallon + "<br/>Quarts: " + quart + <br/>Pints: " + pint + "<br/>Cups: " + cup = "<br/>Fluid Ounces: " + fluidOunces;
-  
-  /////////////////////////////// DO NOT MODIFY
   check("fluidOunces", input); // DO NOT MODIFY
   /////////////////////////////// DO NOT MODIFY
 }
-
 /*
  * Ounces. 5 points.
  */
-
 function ounces() {
-
   /////////////////////////////////////////////////////// DO NOT MODIFY
   let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
   /////////////////////////////////////////////////////// DO NOT MODIFY
-
   // You are free to modify the value of ounces, which you'll likely
   // need to do. Please do not modify the value of input.
-
   ////////////////////// DO NOT MODIFY
   let ounces = input; // DO NOT MODIFY
   ////////////////////// DO NOT MODIFY
-
   // WRITE YOUR EXERCISE 8 CODE HERE
   let tons=Math.floor(ounces/32000)
   let punds= Math.foor((ounces - (tons*32000))/16);
   ounces= Math.floor((ounces - (tons*32000) - (pounds*16))/1);
   document.getElementById("output8").innerHTML="Tons: " + tons + "<br/>Pounds: " + pounds + "<br/>Ounces: " + ounces;
-  
 
   ////////////////////////// DO NOT MODIFY
   check("ounces", input); // DO NOT MODIFY
   ////////////////////////// DO NOT MODIFY
 }
-
 /*
  * Money. 7 points.
  */
-
 function money() {
-
   /////////////////////////////////////////////////////// DO NOT MODIFY
   let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
   /////////////////////////////////////////////////////// DO NOT MODIFY
-
   // You are free to modify the value of pennies, which you'll likely
   // need to do. Please do not modify the value of input.
-
   /////////////////////// DO NOT MODIFY
   let pennies = input; // DO NOT MODIFY
   /////////////////////// DO NOT MODIFY
-
   // WRITE YOUR EXERCISE 9 CODE HERE
   let dollar=Math.floor((pennies)/100);
-  let quarter=Math.floor((pennies - (dollar*100))/25);
-  let dime=Math.floor((pennies - (dollar*100) - (quarter*25))/10);
-  let nickel=Math.floor((pennies - (dollar*100) - (quarter*25) - (dime*10))/5);
-  pennies=Math.floor((pennies - (dollar*100) - (quarter*25) - (dime*10) - (nickel*5))/1);
-  document.getElementById("output9").innerHTML=Dollars: " + dollar + "<br/>Quarters: " + quarter + "<br/>Dimes: " + dime + "<br/>Nickels: " + nickel + "<br/>Pennies: " + pennies;
-                         
+ let quarter=Math.floor((pennies - (dollar*100))/25);
+ let dime=Math.floor((pennies - (dollar*100) - (quarter*25))/10);
+ let nickel=Math.floor((pennies - (dollar*100) - (quarter*25) - (dime*10))/5);
+ pennies=Math.floor((pennies - (dollar*100) - (quarter*25) - (dime*10) - (nickel*5))/1);
+ document.getElementById("output9").innerHTML=Dollars: " + dollar + "<br/>Quarters: " + quarter + "<br/>Dimes: " + dime + "<br/>Nickels: " + nickel + "<br/>Pennies: " + pennies;
+
+
 
   ///////////////////////// DO NOT MODIFY
   check("money", input); // DO NOT MODIFY
@@ -242,14 +200,46 @@ function change() {
   ////////////////////// DO NOT MODIFY
 
   // WRITE YOUR EXERCISE 10 CODE HERE
-let quarter=Math.floor((amount)/25);
+  let quarter=Math.floor((amount)/25);
 let dime=Math.floor((amount - (quarter*25))/10);
-let nickel=Math.floor((amount - (quarter*25) - (dime*10))/5); 
+let nickel=Math.floor((amount - (quarter*25) - (dime*10))/5);
 let pennies=Math.floor((amount - (quarter*25) - (dime*10) - (nickel*5))/1);
 let coins=quarter+dime+nickel+pennies
 
 if(coins=1) {
    document.getElementById("output10").innerHTML=coins + "coins.";
+  ////////////////////////// DO NOT MODIFY
+  check("change", input); // DO NOT MODIFY
+  ////////////////////////// DO NOT MODIFY
+}
+  ////////////////////////// DO NOT MODIFY
+}
+
+
+  ///////////////////////// DO NOT MODIFY
+  check("money", input); // DO NOT MODIFY
+  ///////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Change. 8 points.
+ */
+
+function change() {
+
+  ///////////////////////////////////////////////////////////////////// DO NOT MODIFY
+  let input = prompt("Enter a non-negative number less than 1.00."); // DO NOT MODIFY
+  ///////////////////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // You are free to modify the value of amount, which you'll likely
+  // need to do. Please do not modify the value of input.
+
+  ////////////////////// DO NOT MODIFY
+  let amount = input; // DO NOT MODIFY
+  ////////////////////// DO NOT MODIFY
+
+  // WRITE YOUR EXERCISE 10 CODE HERE
+
   ////////////////////////// DO NOT MODIFY
   check("change", input); // DO NOT MODIFY
   ////////////////////////// DO NOT MODIFY
